f8dd6bb710144ebabc57c5337fcdbb5b5bd432eb
Caleb Druckemiller <cldruckemiller@my.harrisburgu.edu>
2021-02-01 13:17:04 -0500

    Add example 3.2 Maclaurin series expansion

--- example_problems.py
+++ example_problems.py
@@ -1,6 +1,12 @@
-from numerical_method import eulers_method, true_error, true_percent_relative_error
-#Example 1.1 & 1.2
+import math
+from numerical_method import *
 
+def breakpoint():
+    print("\n","*"*50,"\n")
+
+
+#Example 1.1 & 1.2 Numerical & Analytical Solutions to Falling Parachutist
+ 
 print("Example 1.1 & 1.2: Numerical & Analytical Solutions to Falling Parachutist")
 
 g = 9.81 #Gravity
@@ -27,8 +33,8 @@ print(times)
 print(data)
     
 
-print("\n","*"*50,"\n")
-#Example 3.1
+breakpoint()
+#Example 3.1 Calculation of Errors
 #a
 #bridge:
 print("Example 3.1: Calculation of Errors")
@@ -37,3 +43,36 @@ print("Rivet: ", true_error(10, 9))
 print("Bridge: ", true_percent_relative_error(true_error(10_000, 9_999), 10_000))
 print("Rivet: ", true_percent_relative_error(true_error(10, 9), 10))
 
+
+breakpoint()
+
+
+#Example 3.2 Error Estimates for Iterative Methods
+#Maclaurin Series Expansion
+
+#Error Criteria
+error_threshold = .5 * 10**(2-3)
+print("Error Threshold: " , str(error_threshold)+"%")
+
+true_value = pow(math.e, .5)
+
+tpre = true_percent_relative_error(true_value, 1.5)
+
+print(str(tpre) + "%")
+
+approx_error = iterative_relative_percent_approx_error(1.5, 1)
+print(str(approx_error) + "%")
+
+# 33.3% > .05% so we continue
+
+cnt = 2 # We are starting after our first series expansions
+current_approx = 1.5 #This will get updated in our first pass
+previous_approx = 1.5 #This will get updated after our first pass
+
+while approx_error > error_threshold:
+    current_approx += (.5**cnt)/math.factorial(cnt)
+    approx_error = iterative_relative_approx_error(current_approx, previous_approx)*100
+    previous_approx = current_approx
+    cnt+=1
+    print("Approx Error:", str(approx_error) + "%", "\nApproximate Value: ", current_approx, "\nIterations: ", cnt, "\n" )
+    
--- numerical_method.py
+++ numerical_method.py
@@ -5,14 +5,26 @@ def eulers_method(old_value, slope, step):
     return old_value + slope*step
 
 
-def true_factional_relative_error(error,value):
-    return error/value if error < value else value/error
+def true_error(exact, approx):
+    return exact - approx
 
 
-def true_percent_relative_error(error,value):
-    return true_factional_relative_error(error, value) * 100
+def true_percent_relative_error(exact, approx):
+    return true_error(exact, approx)/exact * 100
 
 
-def true_error(exact, approximation):
-    return exact - approximation
+def approx_relative_error(approx_error, approx):
+    return approx_error/approx if approx_error < approx else approx/approx_error
+
+
+def approx_relative_percent_error(approx_error, approx):
+    return approx_relative_error(approx_error, approx) * 100
+
+
+def iterative_relative_approx_error(current_approx, previous_approx):
+    return abs((current_approx - previous_approx) / current_approx)
+
+
+def iterative_relative_percent_approx_error(current_approx, previous_approx):
+    return iterative_relative_approx_error(current_approx, previous_approx) * 100
 
